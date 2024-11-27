1a9128b3cd8285b7bc13f2a0b291c7b2fbb3ddc8
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-11-27 19:30:43 +0700

    Change project name

--- CauTrucLap/Program.cs
+++ CauTrucLap/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CauTrucLap
+﻿
+namespace B3_CauTrucLap
 {
     internal class Program
     {
--- CauTrucLuaChon/Init.cs
+++ CauTrucLuaChon/Init.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CauTrucLuaChon
+namespace B2_CauTrucLuaChon
 {
     internal static class Init
     {
