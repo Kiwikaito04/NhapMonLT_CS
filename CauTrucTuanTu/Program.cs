eb8d99117a3a0d0903da423dc39d1feda978acf4
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-11-29 09:50:02 +0700

    Load Init TH01

---
+++ CauTrucTuanTu/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1_CauTrucTuanTu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BAI 1
+            //Console.Write("Enter your name: ");
+            //Console.WriteLine("Hello, {0}",Console.ReadLine());
+
+            //BAI 2
+            //Console.Write("Input string: ");
+            //string s=Console.ReadLine();
+            //int i = int.Parse(s);
+            //Console.WriteLine("\"stringNumber\" value & type: {0}, {1}\n" +
+            //    "\"intNumber\" value & type: {2}, {3}",s,s.GetType(),i,i.GetType());
+
+            //BAI 3
+            //int a, b;
+            //Console.Write("a = "); 
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("b = "); 
+            //b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("{0} + {1} = {2}\n" +
+            //    "{0} - {1} = {3}\n" +
+            //    "{0} * {1} = {4}\n" +
+            //    "{0} / {1} = {5}\n" +
+            //    "{0} % {1} = {6}", a,b,a+b,a-b,a*b,a/b,a%b);
+
+            //BAI 4
+            //double a, b;
+            //Console.Write("a = ");
+            //a = double.Parse(Console.ReadLine());
+            //Console.Write("b = ");
+            //b = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Before swapping: a = {0}, b = {1}",a,b);
+            //double c = a;
+            //a = b;
+            //b = c;
+            //Console.WriteLine("After swapping: a = {0}, b = {1}",a,b);
+
+            //BAI 5
+            //double r;
+            //Console.Write("r = ");
+            //r = double.Parse(Console.ReadLine());
+            //Console.WriteLine("P = {0:0.00}\n" +
+            //    "A = {1:0.00}",2*Math.PI*r,Math.PI*r*r);
+
+            //BAI 6
+            //int n;
+            //Console.Write("n = ");
+            //n = int.Parse(Console.ReadLine());
+            //for(int i=1;i<=10;i++)
+            //    Console.WriteLine("{0} x {1} = {2}",n,i,n*i);
+
+            //BAI 7
+            //Console.Write("Working hours = ");
+            //double h = double.Parse(Console.ReadLine());
+            //Console.Write("Unit price = ");
+            //int p = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Your Salary = {0:0.00}",p*h);
+
+            //BAI 8
+            //Console.Write("Amount of money: ");
+            //int money = int.Parse(Console.ReadLine());
+            //int[] cash = new int[7] { 100, 50, 20, 10, 5, 2, 1 };
+            //for(int i=0;i<=6;i++)
+            //{
+            //    Console.WriteLine("{0} : {1}", cash[i], money / cash[i]);
+            //    money %= cash[i];
+            //}
+
+            //BAI 9
+            //Console.Write("Input time (s): ");
+            //int s = int.Parse(Console.ReadLine());
+            //int m = s / 60;
+            //s %= 60;
+            //int h = m / 60;
+            //m %= 60;
+            //Console.WriteLine("{0:00} : {1:00} : {2:00}",h,m,s);
+
+            //BAI 10
+            //Console.Write("Attendant mark: ");
+            //double am = double.Parse(Console.ReadLine());
+            //Console.Write("Practical mark: ");
+            //double pm = double.Parse(Console.ReadLine());
+            //Console.Write("Final exam: ");
+            //double fe = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Average mark: {0:0.0}",am*0.2+pm*0.3+fe*0.5);
+        }
+    }
+}
